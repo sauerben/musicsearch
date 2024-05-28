98facea171947c60cc76fc48bae090785fdafe8e
Ben Sauer <bs@marmalade.group>
2024-05-28 08:44:41 +0200

    fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { SearchBar } from "@/components/SeachBar";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-16 max-w-6xl w-full">
-      <SearchBar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SearchBar />
+      </Suspense>
     </main>
   );
 }
